bc26dc8a289464481b33d7108d3e08cfd583bc28
Avinash Tahakik <avinash.tahakik@imgtec.com>
2015-09-24 15:04:08 +0530

    Add command line options to pass url and ping tries for ethernet and wifi script
    [git-p4: depot-paths = "//imgsystems/caskeid_embedded/packages/board_test/MAIN/": change = 3732135]

--- src/test_ethernet.sh
+++ src/test_ethernet.sh
@@ -1,4 +1,4 @@
-# This script will test ethernet by pinging to www.google.com
+# This script will test ethernet by pinging www.google.com else any url provided using -u option
 
 LOG_LEVEL=1
 HOST=www.google.com
@@ -7,9 +7,40 @@ TRIALS=50
 PASS_PERCENTAGE_THRESHOLD=95
 
 source common.sh
-parse_command_line $@
-redirect_output_and_error $LOG_LEVEL
 
+usage()
+{
+cat << EOF
+
+usage: $0 options
+
+OPTIONS:
+-h	Show this message
+-u	Url/IP to ping e.g -u www.wikipedia.org or -u 192.18.95.80
+-c	Number of times to ping e.g -c 50
+-v	Verbose
+
+EOF
+}
+
+while getopts "u:c:vh" opt; do
+	case $opt in
+		u)
+			HOST=$OPTARG;;
+		c)
+			TRIALS=$OPTARG;;
+		v)
+			LOG_LEVEL=2;;
+		h)
+			usage
+			exit 0;;
+		\?)
+			usage
+			exit 1;;
+	esac
+done
+
+redirect_output_and_error $LOG_LEVEL
 echo -e "\n******************************* Ethernet test *********************************\n" >&3
 
 ifconfig $INTERFACE up
@@ -29,6 +60,8 @@ if [[ "$IP_ASSIGNED" = "false" ]];then
 	udhcpc -i $INTERFACE
 fi
 
+echo -e "Pinging to $HOST $TRIALS number of times" >&3
+
 get_ping_percentage ipv4 $INTERFACE $HOST $TRIALS
 PASS_PERCENTAGE=$?
 if [ $PASS_PERCENTAGE -gt $PASS_PERCENTAGE_THRESHOLD ]; then
@@ -38,4 +71,3 @@ else
     echo -e "FAIL, pass percent not more than $PASS_PERCENTAGE_THRESHOLD%\n" >&3
     exit 1
 fi
-
--- src/test_wifi.sh
+++ src/test_wifi.sh
@@ -1,4 +1,4 @@
-# This script will test wifi by pinging www.google.com
+# This script will test wifi by pinging www.google.com else any url provided using -u option
 # Export WLAN_SSID and WLAN_PASSWORD to run this test
 
 LOG_LEVEL=1
@@ -8,18 +8,49 @@ TRIALS=50
 PASS_PERCENTAGE_THRESHOLD=95
 
 source common.sh
-parse_command_line $@
+usage()
+{
+cat << EOF
+
+usage: $0 options
+
+OPTIONS:
+-h	Show this message
+-u	Url/IP to ping e.g -u www.wikipedia.org or -u 192.18.95.80
+-c	Number of times to ping e.g -c 50
+-v	Verbose
+
+EOF
+}
+
+while getopts "u:c:vh" opt; do
+	case $opt in
+		u)
+			HOST=$OPTARG;;
+		c)
+			TRIALS=$OPTARG;;
+		v)
+			LOG_LEVEL=2;;
+		h)
+			usage
+			exit 0;;
+		\?)
+			usage
+			exit 1;;
+	esac
+done
+
 redirect_output_and_error $LOG_LEVEL
 
 echo -e "\n******************************* Wifi test *************************************\n" >&3
 
 if [ -z ${WLAN_SSID} ];then
-	echo "Please export WLAN_SSID to run this script" >&3
+	echo "Please export WLAN_SSID to run this script e.g export WLAN_SSID=ABCD" >&3
 	exit 1
 fi
 
 if [ -z ${WLAN_PASSWORD} ];then
-	echo "Please export WLAN_PASSWORD to run this script" >&3
+	echo "Please export WLAN_PASSWORD to run this script e.g export WLAN_PASSWORD=xyz" >&3
 	exit 1
 fi
 
@@ -35,6 +66,8 @@ if [ $WLAN_STATUS -eq 0 ];then
 	sleep 2 &&\
 	udhcpc -i $INTERFACE
 fi
+
+echo -e "Pinging to $HOST $TRIALS number of times" >&3
 
 if [ $? == 0 ];then
 	get_ping_percentage ipv4 $INTERFACE $HOST $TRIALS
