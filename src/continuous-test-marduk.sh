148f46f10467769bc954258c8258ee35abd6d7a6
Leon Alrae <leon.alrae@imgtec.com>
2015-12-17 12:34:37 +0000

    allow the user to select which continuous tests to run
    User can pass test list to the script. Example:
    
    To start only Heartbeat and 6LoWPAN tests:
    ./continuous-test-marduk.sh hb 6l
    
    Output:
    (...)
    heartbeat P: 275 F: 0 | lowpan0 P: 62 F: 1 |
    heartbeat P: 288 F: 0 | lowpan0 P: 68 F: 1 |
    heartbeat P: 307 F: 0 | lowpan0 P: 70 F: 1 |
    
    Change-Id: I07e64af4f49c2de2069aa5847bac1c2746379ef6
    Signed-off-by: Leon Alrae <leon.alrae@imgtec.com>
    Signed-off-by: Pratik Prajapati <Pratik.Prajapati@imgtec.com>

--- src/continuous-test-marduk.sh
+++ src/continuous-test-marduk.sh
@@ -12,6 +12,21 @@
 # WD4 8LZ, U.K.
 
 # This script will try to run scripts in parallel.
+# Input arguments specify test list to run. If no arguments are provided
+# then running all the tests.
+#
+# Example to start only Heartbeat and 6LoWPAN tests:
+# ./continuous-test-marduk.sh hb 6l
+
+# Test list:
+# au - audio
+# hb - heartbeat
+# sd - SD card
+# et - ethernet
+# wf - WiFi
+# 6l - 6LoWPAN
+
+TESTLIST="au hb sd et wf 6l"
 
 # source any environment variables if set by tester
 source ./common.sh
@@ -25,7 +40,20 @@ usage()
 {
 cat << EOF
 
-usage: $0 options
+usage: $0 [options] [tests]
+Give input arguments to specify test from following test list to run.
+If no arguments are provided then all the tests will be started.
+
+Example to start only Heartbeat and 6LoWPAN tests:
+$0 hb 6l
+
+Test list:
+au - audio
+hb - heartbeat
+sd - SD card
+et - ethernet
+wf - WiFi
+6l - 6LoWPAN
 
 OPTIONS:
 -h	Show this message
@@ -75,32 +103,85 @@ done
 
 kill_all_tests
 
-# starting test scripts in background
-./test_audio.sh -d hw:0,2 -c 0 &
-sleep 2
+# Check if provided tests are valid
+for testname in "$@"
+do
+	FOUND=false
+	for t in $TESTLIST
+	do
+		if [ "$testname" == "$t" ]; then
+			FOUND=true
+			break
+		fi
+	done
+	if (!("$FOUND")); then
+		echo "Error! Unknown test: $testname"
+		exit 1
+	fi
+done
 
-./test_heartbeat_led.sh -c 0 &
-sleep 2
+# Delete previous test results.
+rm /tmp/cont_board_test.*
 
-./test_sdcard.sh -c 0 &
-sleep 2
+start_bg_test_au()
+{
+	./test_audio.sh -d hw:0,2 -c 0 &
+	sleep 2
+}
 
-if [ -z ${ETHERNET_PING_HOST} ];then
-	./test_ethernet.sh -c 0 &
-else
-	./test_ethernet.sh -u $ETHERNET_PING_HOST -c 0 &
+start_bg_test_hb()
+{
+	./test_heartbeat_led.sh -c 0 &
+	sleep 2
+}
+
+start_bg_test_sd()
+{
+	./test_sdcard.sh -c 0 &
+	sleep 2
+}
+
+start_bg_test_et()
+{
+	if [ -z ${ETHERNET_PING_HOST} ];then
+		./test_ethernet.sh -c 0 &
+	else
+		./test_ethernet.sh -u $ETHERNET_PING_HOST -c 0 &
+	fi
+	sleep 2
+}
+
+start_bg_test_wf()
+{
+	if [ -z ${WIFI_PING_HOST} ];then
+		./test_wifi.sh -c 0 &
+	else
+		./test_wifi.sh -u $WIFI_PING_HOST -c 0 &
+	fi
+	sleep 2
+}
+
+start_bg_test_6l()
+{
+	./test_6lowpan.sh -c 0 &
+	sleep 2
+}
+
+# Start selected tests
+TEST_STARTED=false
+for testname in "$@"
+do
+	start_bg_test_$testname
+	TEST_STARTED=true
+done
+
+if (!("$TEST_STARTED")); then
+	echo "No argument specified, thus running all the tests"
+	for t in $TESTLIST
+	do
+		start_bg_test_$t
+	done
 fi
-sleep 2
-
-if [ -z ${WIFI_PING_HOST} ];then
-	./test_wifi.sh -c 0 &
-else
-	./test_wifi.sh -u $WIFI_PING_HOST -c 0 &
-fi
-sleep 2
-
-./test_6lowpan.sh -c 0 &
-sleep 2
 
 # wait until tests start running before printing out results
 sleep 30
