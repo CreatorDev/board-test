1558e3d4876fb1693231d5ba1911919b5d3a0102
Mayank Sirotiya <Mayank.Sirotiya@imgtec.com>
2016-01-14 15:44:02 +0530

    add continuous mode support for combined bargraph and thermo3 click test
    Change-Id: Ib142401c6301730c95c641d1f66103deb063a881
    Signed-off-by: Mayank Sirotiya <Mayank.Sirotiya@imgtec.com>

--- src/click/test_click_thermo3_to_bargraph.sh
+++ src/click/test_click_thermo3_to_bargraph.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 #
 # Copyright 2015 by Imagination Technologies Limited and/or its affiliated group companies.
 #
@@ -22,13 +23,50 @@
 # is 26 and specified base is 20, then 6 display segments will be lit up.
 
 LOG_LEVEL=1
+TRIALS=1
+
+MARGIN_BOT=20
+SEGMENTS=10
+
+usage()
+{
+cat << EOF
+
+usage: $0 options
+
+OPTIONS:
+-h	Show this message
+-c	Number of trials, default 1, and pass -c 0 for continuous mode
+-t	Base temperature, default 20
+-v	Verbose
+-V	Show package version
+
+EOF
+}
+
+while getopts "c:t:vVh" opt; do
+	case $opt in
+		c)
+			TRIALS=$OPTARG;;
+		t)
+			MARGIN_BOT=$OPTARG;;
+		v)
+			LOG_LEVEL=2;;
+		V)
+			echo -n "version = "
+			cat version
+			exit 0;;
+		h)
+			usage
+			exit 0;;
+		\?)
+			usage
+			exit 1;;
+	esac
+done
 
 source click_common.sh
-parse_command_line $@
 redirect_output_and_error $LOG_LEVEL
-
-MARGIN_BOT=$1
-SEGMENTS=10
 
 echo -e "\n**************************  CLICK THERMO3 + BARGRAPH combined test **************************\n" >&3
 
@@ -41,21 +79,55 @@ fi
 echo -e "base temperature: $MARGIN_BOT\n"
 
 enable_i2c_driver
-
-VAL=`./test_click_read_thermo3 -m 2`
-echo -e "measured temperature: $VAL\n"
-# drop fraction
-VAL=${VAL%.*}
-
-MARGIN_TOP=$((MARGIN_BOT+SEGMENTS))
-if [ "$VAL" -ge "$MARGIN_TOP" ]; then ACTIVE=10
-elif [ "$VAL" -le "$MARGIN_BOT" ]; then ACTIVE=0
-else ACTIVE=$((VAL - MARGIN_BOT))
-fi
-
 enable_pwm 1
 
-./test_click_write_bargraph -m 1 -s "$(( (1<<ACTIVE) - 1 ))"
+read_and_update_bargraph()
+{
+	VAL=`./test_click_read_thermo3 -m 2`
+	if [ $? -ne 0 ];then
+		return 1
+	fi
 
-echo -e "Are there $ACTIVE active segments?\n" >&3
-show_result_based_on_switch_pressed
+	echo -e "measured temperature: $VAL\n"
+	# drop fraction
+	VAL=${VAL%.*}
+
+	MARGIN_TOP=$((MARGIN_BOT+SEGMENTS))
+	if [ "$VAL" -ge "$MARGIN_TOP" ]; then ACTIVE=10
+	elif [ "$VAL" -le "$MARGIN_BOT" ]; then ACTIVE=0
+	else ACTIVE=$((VAL - MARGIN_BOT))
+	fi
+
+	./test_click_write_bargraph -m 1 -s "$(( (1<<ACTIVE) - 1 ))"
+	if [ $? -ne 0 ];then
+		return 1
+	fi
+}
+
+if [ $TRIALS -eq 0 ];then
+	echo -e "Running test continuously\n" >&3
+	PASS=0
+	FAIL=0
+	while true
+	do
+		read_and_update_bargraph
+		if [ $? -eq 0 ];then
+			PASS=$((PASS + 1))
+		else
+			FAIL=$((FAIL + 1))
+		fi
+		update_test_status "thermo3_bargraph" 2 $PASS $FAIL
+		sleep 1
+	done
+else
+	echo -e "Running test for $TRIALS trials\n" >&3
+	for j in $(seq 1 $TRIALS)
+	do
+		read_and_update_bargraph
+		echo -e "Bargraph updated\n" >&3
+		sleep 1
+	done
+
+	echo -e "Are there $ACTIVE active segments?\n" >&3
+	show_result_based_on_switch_pressed
+fi
