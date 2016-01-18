8873cc3a0f8b02c76bc19914d322b2513aebc010
Manohar Narkhede <Manohar.Narkhede@imgtec.com>
2016-01-18 12:38:36 +0530

    add logic in sdcard script to detect storage device on the board.
    Change-Id: I4a0de9c9c4db94cc689fede8701e254ac8ebe50d
    Signed-off-by: Manohar Narkhede <Manohar.Narkhede@imgtec.com>

--- src/test_sdcard.sh
+++ src/test_sdcard.sh
@@ -19,8 +19,6 @@
 LOG_LEVEL=1
 TRIALS=1
 CONTINUOUS=false
-TEST_DEVICE=mmcblk0p
-SEARCH_CMD="grep $TEST_DEVICE"
 
 source common.sh
 
@@ -33,22 +31,18 @@ usage: $0 options
 OPTIONS:
 -h	Show this message
 -c	Number of trials, default 1, and pass -c 0 for continuous mode
--e	Test eMMc
 -v	Verbose
 -V	Show package version
 
 EOF
 }
 
-while getopts "c:veVh" opt; do
+while getopts "c:vVh" opt; do
 	case $opt in
 		c)
 			TRIALS=$OPTARG;;
 		v)
 			LOG_LEVEL=2;;
-		e)
-			TEST_DEVICE=mmcblk0
-			SEARCH_CMD="grep $TEST_DEVICE -w";;
 		V)
 			echo -n "version = "
 			cat version
@@ -66,7 +60,23 @@ redirect_output_and_error $LOG_LEVEL
 
 echo -e "\n******************************* SD card/emmc test **********************************\n" >&3
 
+#Detect storage device on the board
+SEARCH_CMD="grep mmcblk0p"
 TOTAL_MMC_DEVS=`ls /dev | $SEARCH_CMD -c`
+if [ $TOTAL_MMC_DEVS -ne 0 ];then
+	echo -e "SD card found on the board" >&3
+	TEST_DEVICE=sdcard
+else
+	SEARCH_CMD="grep mmcblk0 -w"
+	TOTAL_MMC_DEVS=`ls /dev | $SEARCH_CMD -c`
+	TEST_DEVICE=eMMC
+	if [ $TOTAL_MMC_DEVS -ne 0 ];then
+		echo -e "eMMC found on the board" >&3
+	else
+		echo "FAIL - partitions not found" >&3
+		exit 1
+	fi
+fi
 
 create_mountpoints()
 {
@@ -89,51 +99,44 @@ create_mountpoints()
 	done
 }
 
-if [ $TOTAL_MMC_DEVS -ne 0 ];then
+create_mountpoints $TOTAL_MMC_DEVS
 
-	create_mountpoints $TOTAL_MMC_DEVS
-
-	#check if we are asked to run continuously
-	if [ $TRIALS -eq 0 ];then
-		CONTINUOUS=true
-	fi
-
-	PASS=0
-	FAIL=0
-	while [ $CONTINUOUS == true -o $TRIALS -gt 0 ]
-	do
-		COUNT=$TOTAL_MMC_DEVS
-		while [ $COUNT -gt 0 ]
-		do
-			COUNT=$((COUNT-1))
-			NUM=$(($TOTAL_MMC_DEVS-$COUNT))
-			MMC_DEVICE=`ls /dev/ | $SEARCH_CMD | head -n $NUM | tail -n 1`
-			MOUNT_DIR=/mnt/$MMC_DEVICE
-
-			if mountpoint -q -- $MOUNT_DIR;then
-				dd if=/dev/urandom of=/tmp/temp0.img bs=1k count=1024	&&\
-				cp /tmp/temp0.img $MOUNT_DIR/temp1.img	&&\
-				sync	&&\
-				cmp /tmp/temp0.img $MOUNT_DIR/temp1.img
-				if [ $? == 0 ]; then
-					echo "PASS"
-					PASS=$((PASS + 1))
-				else
-					echo "FAIL - partition $MMC_DEVICE"
-					FAIL=$((FAIL + 1))
-					if [ $COUNT == 0 ]; then
-						exit 1
-					fi
-				fi
-				[ $CONTINUOUS == true ] && update_test_status "$TEST_DEVICE" 2 $PASS $FAIL
-				rm /tmp/temp0.img $MOUNT_DIR/temp1.img >&4
-			fi
-		done
-		TRIALS=$(($TRIALS-1))
-		sleep 1
-	done
-
-else
-	echo "FAIL - partitions not found" >&3
-	exit 1
+#check if we are asked to run continuously
+if [ $TRIALS -eq 0 ];then
+	CONTINUOUS=true
 fi
+
+PASS=0
+FAIL=0
+while [ $CONTINUOUS == true -o $TRIALS -gt 0 ]
+do
+	COUNT=$TOTAL_MMC_DEVS
+	while [ $COUNT -gt 0 ]
+	do
+		COUNT=$((COUNT-1))
+		NUM=$(($TOTAL_MMC_DEVS-$COUNT))
+		MMC_DEVICE=`ls /dev/ | $SEARCH_CMD | head -n $NUM | tail -n 1`
+		MOUNT_DIR=/mnt/$MMC_DEVICE
+
+		if mountpoint -q -- $MOUNT_DIR;then
+			dd if=/dev/urandom of=/tmp/temp0.img bs=1k count=1024	&&\
+			cp /tmp/temp0.img $MOUNT_DIR/temp1.img	&&\
+			sync	&&\
+			cmp /tmp/temp0.img $MOUNT_DIR/temp1.img
+			if [ $? == 0 ]; then
+				echo "PASS"
+				PASS=$((PASS + 1))
+			else
+				echo "FAIL - partition $MMC_DEVICE"
+				FAIL=$((FAIL + 1))
+				if [ $COUNT == 0 ]; then
+					exit 1
+				fi
+			fi
+			[ $CONTINUOUS == true ] && update_test_status "$TEST_DEVICE" 2 $PASS $FAIL
+			rm /tmp/temp0.img $MOUNT_DIR/temp1.img >&4
+		fi
+	done
+	TRIALS=$(($TRIALS-1))
+	sleep 1
+done
