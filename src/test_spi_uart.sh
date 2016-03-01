1513905e4cf9a1cc7c3f019e04e42aa26a85b8c9
Francois Berder <Francois.Berder@imgtec.com>
2016-03-01 15:20:47 +0000

    add spi-uart test in continuous test for marduk
    Change-Id: I57adef9f108d7517055d13be592bbfc65ef18160
    Signed-off-by: Francois Berder <Francois.Berder@imgtec.com>

--- src/test_spi_uart.sh
+++ src/test_spi_uart.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 #
 # Copyright 2015 by Imagination Technologies Limited and/or its affiliated group companies.
 #
@@ -15,29 +16,80 @@
 
 LOG_LEVEL=1
 BLINK_DELAY_USEC=50000
+TRIALS=1
+CONTINUOUS=false
 
 source common.sh
-parse_command_line $@
+
+usage()
+{
+cat << EOF
+
+usage: $0 options
+
+OPTIONS:
+-h	Show this message
+-c	Number of times to run test, default 1, and pass -c 0 for continuous mode
+EOF
+}
+
+while getopts "c:vVh" opt; do
+	case $opt in
+		c)
+			TRIALS=$OPTARG;;
+		v)
+			LOG_LEVEL=2;;
+		V)
+			echo -n "version = "
+			cat version
+			exit 0;;
+		h)
+			usage
+			exit 0;;
+		\?)
+			usage
+			exit 1;;
+	esac
+done
+
 redirect_output_and_error $LOG_LEVEL
 
 echo -e "\n**************************  SPI-UART chip test **************************\n" >&3
 
-# sc16is7xx has 8 GPIO's, they get added from 504 to 511, 511 is connected to click power enable
-# while others are connected to LED
-echo -e "Enable Click Power \n"
-CLICK_POWER_GPIO=511
+run_test()
+{
+	# sc16is7xx has 8 GPIO's, they get added from 504 to 511, 511 is connected to click power enable
+	# while others are connected to LED
+	echo -e "Enable Click Power \n"
+	CLICK_POWER_GPIO=511
 
-sh test_set_pin.sh $CLICK_POWER_GPIO 1
+	sh test_set_pin.sh $CLICK_POWER_GPIO 1
 
-echo -e "Blinking all Led's \n"
+	echo -e "Blinking all Led's \n"
 
-for i in 504 505 506 507 508 509 510
+	for i in 504 505 506 507 508 509 510
+	do
+		sh test_set_pin.sh $i 1
+		usleep $BLINK_DELAY_USEC
+		sh test_set_pin.sh $i 0
+		usleep $BLINK_DELAY_USEC
+	done
+
+	# Cannot switch button if run from continous-test-marduk script
+	if [ "$CONTINUOUS" = false ]; then
+		echo -e "\nDid all the LED's blink?\n" >&3
+		show_result_based_on_switch_pressed
+	fi
+}
+
+if [ $TRIALS -eq 0 ]; then
+	CONTINUOUS=true
+fi
+
+while [ "$CONTINUOUS" = true -o $TRIALS -gt 0 ]
 do
-	sh test_set_pin.sh $i 1
-	usleep $BLINK_DELAY_USEC
-	sh test_set_pin.sh $i 0
-	usleep $BLINK_DELAY_USEC
+	run_test
+	TRIALS=$(($TRIALS-1))
+	sleep 1
 done
 
-echo -e "\nDid all the LED's blink?\n" >&3
-show_result_based_on_switch_pressed
